6e9f6ed6cc179668ef1a6411bc7f772c05dbcbdc
pelikhan <pelikhan@users.noreply.github.com>
2021-03-09 13:15:57 +0000

    deploy: cd24d6ff7057b5ba0214a453a9408db35dbf7d02

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-23f47f231ca908aecfda.js"
+    "url": "webpack-runtime-13ae9cca202102698d8a.js"
   },
   {
     "url": "framework-d2b9019ed825b9acf8aa.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e2f5af7c08b0b81e10f58e8f318cd358"
+    "revision": "a8ad263d55098c4bd8c14f1ef456df0c"
   },
   {
     "url": "static/webfonts/s/roboto/v20/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
@@ -72,7 +72,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "5e9785b6fc777b20da2b82875a4f7d12"
+    "revision": "8c36d852545b28d250b83b6ff9e0cbee"
   },
   {
     "url": "polyfill-59ee27a2411ab13ccf1c.js"
@@ -716,7 +716,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/jacdac-docs/app-c94dc7c6b8e5683b13b1.js`))) {
+  if (!resources || !(await caches.match(`/jacdac-docs/app-32c4c53d10cc67ca4636.js`))) {
     return await fetch(event.request)
   }
 
---
+++ webpack-runtime-13ae9cca202102698d8a.js
@@ -0,0 +1,224 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// install a JSONP callback for chunk loading
+/******/ 	function webpackJsonpCallback(data) {
+/******/ 		var chunkIds = data[0];
+/******/ 		var moreModules = data[1];
+/******/ 		var executeModules = data[2];
+/******/
+/******/ 		// add "moreModules" to the modules object,
+/******/ 		// then flag all "chunkIds" as loaded and fire callback
+/******/ 		var moduleId, chunkId, i = 0, resolves = [];
+/******/ 		for(;i < chunkIds.length; i++) {
+/******/ 			chunkId = chunkIds[i];
+/******/ 			if(Object.prototype.hasOwnProperty.call(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 				resolves.push(installedChunks[chunkId][0]);
+/******/ 			}
+/******/ 			installedChunks[chunkId] = 0;
+/******/ 		}
+/******/ 		for(moduleId in moreModules) {
+/******/ 			if(Object.prototype.hasOwnProperty.call(moreModules, moduleId)) {
+/******/ 				modules[moduleId] = moreModules[moduleId];
+/******/ 			}
+/******/ 		}
+/******/ 		if(parentJsonpFunction) parentJsonpFunction(data);
+/******/
+/******/ 		while(resolves.length) {
+/******/ 			resolves.shift()();
+/******/ 		}
+/******/
+/******/ 		// add entry modules from loaded chunk to deferred list
+/******/ 		deferredModules.push.apply(deferredModules, executeModules || []);
+/******/
+/******/ 		// run deferred modules when all chunks ready
+/******/ 		return checkDeferredModules();
+/******/ 	};
+/******/ 	function checkDeferredModules() {
+/******/ 		var result;
+/******/ 		for(var i = 0; i < deferredModules.length; i++) {
+/******/ 			var deferredModule = deferredModules[i];
+/******/ 			var fulfilled = true;
+/******/ 			for(var j = 1; j < deferredModule.length; j++) {
+/******/ 				var depId = deferredModule[j];
+/******/ 				if(installedChunks[depId] !== 0) fulfilled = false;
+/******/ 			}
+/******/ 			if(fulfilled) {
+/******/ 				deferredModules.splice(i--, 1);
+/******/ 				result = __webpack_require__(__webpack_require__.s = deferredModule[0]);
+/******/ 			}
+/******/ 		}
+/******/
+/******/ 		return result;
+/******/ 	}
+/******/
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// object to store loaded and loading chunks
+/******/ 	// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 	// Promise = chunk loading, 0 = chunk loaded
+/******/ 	var installedChunks = {
+/******/ 		2: 0
+/******/ 	};
+/******/
+/******/ 	var deferredModules = [];
+/******/
+/******/ 	// script path function
+/******/ 	function jsonpScriptSrc(chunkId) {
+/******/ 		return __webpack_require__.p + "" + ({"0":"c8f7fe3b0e41be846d5687592cf2018ff6e22687","1":"fb7d5399","4":"component---cache-caches-gatsby-plugin-offline-app-shell-js","5":"component---src-components-spec-tsx","6":"component---src-pages-404-mdx","7":"component---src-pages-clients-hardware-makecode-mdx","8":"component---src-pages-clients-mdx","9":"component---src-pages-clients-web-iframe-mdx","10":"component---src-pages-clients-web-jdom-bus-mdx","11":"component---src-pages-clients-web-jdom-device-mdx","12":"component---src-pages-clients-web-jdom-event-mdx","13":"component---src-pages-clients-web-jdom-field-mdx","14":"component---src-pages-clients-web-jdom-mdx","15":"component---src-pages-clients-web-jdom-node-mdx","16":"component---src-pages-clients-web-jdom-register-mdx","17":"component---src-pages-clients-web-jdom-service-mdx","18":"component---src-pages-clients-web-mdx","19":"component---src-pages-clients-web-react-mdx","20":"component---src-pages-clients-web-setup-mdx","21":"component---src-pages-dashboard-mdx","22":"component---src-pages-devices-mdx","23":"component---src-pages-dtmi-mdx","24":"component---src-pages-github-mdx","25":"component---src-pages-index-mdx","26":"component---src-pages-services-mdx","27":"component---src-pages-tools-azure-device-twin-designer-mdx","28":"component---src-pages-tools-azure-iot-hub-mdx","29":"component---src-pages-tools-collector-mdx","30":"component---src-pages-tools-device-registration-mdx","31":"component---src-pages-tools-edge-impulse-mdx","32":"component---src-pages-tools-jupyterlab-mdx","33":"component---src-pages-tools-led-animation-designer-mdx","34":"component---src-pages-tools-makecode-editor-extension-mdx","35":"component---src-pages-tools-makecode-mdx","36":"component---src-pages-tools-makecode-sim-mdx","37":"component---src-pages-tools-mdx","38":"component---src-pages-tools-model-uploader-mdx","39":"component---src-pages-tools-packet-inspector-mdx","40":"component---src-pages-tools-player-mdx","41":"component---src-pages-tools-prototest-mdx","42":"component---src-pages-tools-role-manager-mdx","43":"component---src-pages-tools-service-editor-mdx","44":"component---src-pages-tools-service-test-editor-mdx","45":"component---src-pages-tools-settings-manager-mdx","46":"component---src-pages-tools-updater-mdx","47":"component---src-pages-traces-mdx","48":"component---src-templates-device-company-mdx","49":"component---src-templates-device-mdx","50":"component---src-templates-service-mdx","51":"component---src-templates-service-playground-mdx","52":"component---src-templates-service-test-mdx"}[chunkId]||chunkId) + "-" + {"0":"c0b4548d858e6ab64d5f","1":"3f9e407ad235dd879b56","4":"b017944d397662186822","5":"231d7eab293edf26f958","6":"d92eec4f29622d368411","7":"e205c7cb9f25b39d8a77","8":"f236ddb40e7bac664800","9":"5f9d23c81a52e7c93d91","10":"854c669b07835aec684f","11":"a784317ba766b8e282aa","12":"69ead9b4832c0284caae","13":"c2914423422c2371bbd6","14":"6085c4e661cee32b1b00","15":"2318168e19a418bd4a03","16":"185b30bc2380f5c65342","17":"1af2f4f4466c653fbe8f","18":"653061a436cd2d993fb6","19":"0ca680a31a94ed17aed9","20":"95903bedc281e27c43d3","21":"ccb340511296319f7643","22":"02886106bff594236055","23":"82a51696e1201f561415","24":"2b612b19b3037ed9c893","25":"51828e9060124481d95b","26":"ec5e50d6fe65eee6e4f4","27":"cd89740eb5e1be4188d2","28":"7378fb195cf334ec32b3","29":"14ef58a3c99136e6921e","30":"8776bc1ae06703c42575","31":"6f65b4053435529069dc","32":"efa740bf68c7977d9171","33":"ab254328e87f0583485a","34":"ccca135638a346125ef1","35":"297664ebd593b28e5598","36":"50522fc60469c1aa0dc1","37":"cff68949109c100c3dd9","38":"519f1b0b7a394a4eff78","39":"3d41a26d10bff8555fbb","40":"72dc7b10fe0bce9de6c9","41":"9e2afe504ca290a0cda4","42":"3e4f2a1d59ed951854da","43":"9ee6fff4132d92900a11","44":"21e943b38f62c52a587c","45":"f31f3c764d297cddcbba","46":"29e9610060c5c0f0ef04","47":"a9dedfac44ecd3a51d61","48":"9a07fb28494c87ca9f10","49":"2d080de324b5031c39c5","50":"9354db20c68cab525f47","51":"183f1ed294ccfff41bcb","52":"5c7482eee15461189b5d","56":"918e2ddcd830f72b077a","57":"d2b0b20b31530371ef64","58":"6024185758eaae6e4486","59":"48cb49e55c281cd48976","60":"d30f0eae44fd862f4ba5","61":"c7e17884cc43bb94f93b","62":"6782d660bd4c51a78aac","63":"907e2dee42b7295bcd4e","64":"4a378a16bc79f5af038b","65":"0395e88f76da1779ad79","66":"7fa0ed47dbc326964128","67":"c2a2e4846b0880b33a2c","68":"c39ced67418e6e0e50d2","69":"af468429bf43d4cad44c","70":"73872afe47344fba17d4","71":"fc4ac282154a145ec343","72":"2e25865de2c0efc74d84","73":"d9e41744de660972fb40","74":"f0267ac7cecc30ccf7af","75":"c523f0200ed9ca575987","76":"c879b23f6259a0c50129","77":"ab503083006b947c4fec","78":"287c6a31d42cea00dbde","79":"383652006a8da4fbf404","80":"ac7f4d4f2d1c284bd96c","81":"b1124fdbead883e45d0f","82":"9bf5d569535558fd81dc","83":"e0d0b1dfc79a634e93e8","84":"601aad1fa3d12270a4da","85":"28bb2f86b28164f28d7e","86":"65fd20a5033445702572","87":"522fb8304ccb8b538817","88":"05de0136138a3c65ecd6","89":"0ae96c3df753932139bc","90":"e94a3b08a4d1faa5e2ed","91":"c86451659da1e8157d6c"}[chunkId] + ".js"
+/******/ 	}
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/ 	// This file contains only the entry chunk.
+/******/ 	// The chunk loading function for additional chunks
+/******/ 	__webpack_require__.e = function requireEnsure(chunkId) {
+/******/ 		var promises = [];
+/******/
+/******/
+/******/ 		// JSONP chunk loading for javascript
+/******/
+/******/ 		var installedChunkData = installedChunks[chunkId];
+/******/ 		if(installedChunkData !== 0) { // 0 means "already installed".
+/******/
+/******/ 			// a Promise means "currently loading".
+/******/ 			if(installedChunkData) {
+/******/ 				promises.push(installedChunkData[2]);
+/******/ 			} else {
+/******/ 				// setup Promise in chunk cache
+/******/ 				var promise = new Promise(function(resolve, reject) {
+/******/ 					installedChunkData = installedChunks[chunkId] = [resolve, reject];
+/******/ 				});
+/******/ 				promises.push(installedChunkData[2] = promise);
+/******/
+/******/ 				// start chunk loading
+/******/ 				var script = document.createElement('script');
+/******/ 				var onScriptComplete;
+/******/
+/******/ 				script.charset = 'utf-8';
+/******/ 				script.timeout = 120;
+/******/ 				if (__webpack_require__.nc) {
+/******/ 					script.setAttribute("nonce", __webpack_require__.nc);
+/******/ 				}
+/******/ 				script.src = jsonpScriptSrc(chunkId);
+/******/
+/******/ 				// create error before stack unwound to get useful stacktrace later
+/******/ 				var error = new Error();
+/******/ 				onScriptComplete = function (event) {
+/******/ 					// avoid mem leaks in IE.
+/******/ 					script.onerror = script.onload = null;
+/******/ 					clearTimeout(timeout);
+/******/ 					var chunk = installedChunks[chunkId];
+/******/ 					if(chunk !== 0) {
+/******/ 						if(chunk) {
+/******/ 							var errorType = event && (event.type === 'load' ? 'missing' : event.type);
+/******/ 							var realSrc = event && event.target && event.target.src;
+/******/ 							error.message = 'Loading chunk ' + chunkId + ' failed.\n(' + errorType + ': ' + realSrc + ')';
+/******/ 							error.name = 'ChunkLoadError';
+/******/ 							error.type = errorType;
+/******/ 							error.request = realSrc;
+/******/ 							chunk[1](error);
+/******/ 						}
+/******/ 						installedChunks[chunkId] = undefined;
+/******/ 					}
+/******/ 				};
+/******/ 				var timeout = setTimeout(function(){
+/******/ 					onScriptComplete({ type: 'timeout', target: script });
+/******/ 				}, 120000);
+/******/ 				script.onerror = script.onload = onScriptComplete;
+/******/ 				document.head.appendChild(script);
+/******/ 			}
+/******/ 		}
+/******/ 		return Promise.all(promises);
+/******/ 	};
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/jacdac-docs/";
+/******/
+/******/ 	// on error function for async loading
+/******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
+/******/
+/******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
+/******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
+/******/ 	jsonpArray.push = webpackJsonpCallback;
+/******/ 	jsonpArray = jsonpArray.slice();
+/******/ 	for(var i = 0; i < jsonpArray.length; i++) webpackJsonpCallback(jsonpArray[i]);
+/******/ 	var parentJsonpFunction = oldJsonpFunction;
+/******/
+/******/
+/******/ 	// run deferred modules from other chunks
+/******/ 	checkDeferredModules();
+/******/ })
+/************************************************************************/
+/******/ ([]);
+//# sourceMappingURL=webpack-runtime-13ae9cca202102698d8a.js.map
